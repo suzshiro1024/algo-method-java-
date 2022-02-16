65f06638b6f7b5ed8b6a0b64f19c6e6a00b845be
Tamamo-Dictus <tix131423113210@gmail.com>
2022-02-16 22:23:30 +0900

    move file

---
+++ 02_std_IO/01_output/02/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args){
+    System.out.println("1");
+    System.out.println("2");
+    System.out.println("3");
+  }
+}
---
+++ 02_std_IO/01_output/04/Main.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args){
+    System.out.println(314*(159+265)-358);
+  }
+}
