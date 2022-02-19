bcc91d903efd66f15a6a14d63c51d684a4e2ea5e
Tamamo-Dictus <tix131423113210@gmail.com>
2022-02-19 19:32:50 +0900

    04-01

---
+++ 04_logical_thinking_training/01_basic_mathematics_1/05/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+    int C = sc.nextInt();
+    int D = sc.nextInt();
+
+    System.out.println(A * C + B * D);
+  }
+}
