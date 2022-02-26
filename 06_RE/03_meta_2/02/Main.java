1b81f9e9f7501b9169f0df539dacfd66336ae103
Tamamo-Dictus <tix131423113210@gmail.com>
2022-02-26 18:01:53 +0900

    06-03

---
+++ 06_RE/03_meta_2/02/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.regex.*;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    Pattern p = Pattern.compile(".*\\d{3,}.*");
+
+    Matcher m = p.matcher(sc.nextLine());
+
+    if(m.matches()){
+      System.out.println("Yes");
+    }else{
+      System.out.println("No");
+    }
+  }
+}
