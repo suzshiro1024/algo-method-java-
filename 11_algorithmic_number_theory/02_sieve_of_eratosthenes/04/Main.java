0f69b55d06ef07229e83db9d3bab0b0a7e96acfa
suzshiro1024 <tix131423113210@gmail.com>
2022-04-11 21:48:09 +0900

    11-02-04

---
+++ 11_algorithmic_number_theory/02_sieve_of_eratosthenes/04/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    int N = Integer.parseInt(sc.next());
+    sc.close();
+
+    boolean[] sieve = new boolean[N+1];
+    Arrays.fill(sieve, true);
+    sieve[0] = false;
+    sieve[1] = false;
+    for(int i=2; i<=N; i++){
+      if(!sieve[i]) continue;
+      for(int j=i*2; j<=N; j+=i){
+        sieve[j] = false;
+      }
+    }
+
+    int ans = 0;
+    for(int i=0; i+2<=N; i++){
+      if(sieve[i] && sieve[i+2])  ans++;
+    }
+    System.out.println(ans);
+  }
+}
