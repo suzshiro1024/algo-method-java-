bf4223408a01806135afda6a09219d833de076fe
suzshiro1024 <tix131423113210@gmail.com>
2022-04-14 23:15:40 +0900

    11-05-03

---
+++ 11_algorithmic_number_theory/05_greatest_common_divisor/03/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    long A = Long.parseLong(sc.next());
+    long B = Long.parseLong(sc.next());
+    sc.close();
+
+    System.out.println(GCD(A,B));
+  }
+
+  public static long GCD(long A, long B){
+    if(B == 0){
+      return A;
+    }
+    return GCD(B, A % B);
+  }
+}
